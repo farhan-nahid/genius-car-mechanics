8150f148544a2d89fb2aaabeb78d66df5e53d001
gesora96 <nahidahmed9933@gmail.com>
2021-11-03 13:43:13 +0600

    experts load from json

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,51 +1,35 @@
-import React from 'react';
-import mechanic1 from '../../../assets/images/mechanic/mechanic-1.jpg';
-import mechanic2 from '../../../assets/images/mechanic/mechanic-2.jpg';
-import mechanic3 from '../../../assets/images/mechanic/mechanic-3.jpg';
-import mechanic4 from '../../../assets/images/mechanic/mechanic-4.jpg';
-import mechanic5 from '../../../assets/images/mechanic/mechanic-5.jpg';
+import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 import Expert from '../Expert/Expert';
 
-const experts = [
-  {
-    img: mechanic1,
-    name: 'Andrew Smith',
-    expertize: '-Engine Expert-',
-  },
-  {
-    img: mechanic2,
-    name: 'John Anderson',
-    expertize: '-Polish Expert-',
-  },
-  {
-    img: mechanic3,
-    name: 'Zakaria Smith',
-    expertize: '-Coloring Expert-',
-  },
-  {
-    img: mechanic4,
-    name: 'Sakib Anderson',
-    expertize: '-Al rounder Expert-',
-  },
-  {
-    img: mechanic5,
-    name: 'John Dou',
-    expertize: '-Tire Expert-',
-  },
-];
-
 const Experts = () => {
+  const [experts, setExperts] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://raw.githubusercontent.com/farhan-nahid/genius-car-mechanics-client/main/src/assets/fakeData/experts.json'
+    )
+      .then((res) => res.json())
+      .then((data) => setExperts(data));
+  }, []);
+
   return (
     <section className='container section__margin' id='experts'>
       <h2 className='section__title'>Our Experts</h2>
-      <div className='grid__container'>
-        {
-          // map experts
-          experts.map((expert, idx) => (
-            <Expert key={idx} expert={expert} />
-          ))
-        }
-      </div>
+      {experts.length ? (
+        <div className='grid__container'>
+          {
+            // map experts
+            experts.map((expert, idx) => (
+              <Expert key={idx} expert={expert} />
+            ))
+          }
+        </div>
+      ) : (
+        <span className='my-5 d-flex justify-content-center py-5'>
+          <Spinner animation='border' role='status' />
+        </span>
+      )}
     </section>
   );
 };
